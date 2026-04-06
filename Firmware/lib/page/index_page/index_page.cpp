3c423dd4a52e8cf2045abe8f58ad7783b0b5b836
AchcarLucas <achcarlucas@gmail.com>
2026-04-06 15:17:47 -0300

    - removido texto 'Index Page'

--- Firmware/lib/page/index_page/index_page.cpp
+++ Firmware/lib/page/index_page/index_page.cpp
@@ -77,7 +77,7 @@ String IndexPage::header() {
             "</head>";
 
     String mode = DEBUG ? "[Debug Mode]" : "[Production Mode]";
-    html += "<header><h2>" + String(TITLE) + " - Univesp - PI V</h2><h3 style=\"text-align: center;\">" + mode + " Index Page</h3></header>";
+    html += "<header><h2>" + String(TITLE) + " - Univesp - PI V</h2><h3 style=\"text-align: center;\">" + mode + "</h3></header>";
 
     return html;
 }
