14c7df33e6047164f9e42688f9608e380c81bb69
AchcarLucas <achcarlucas@gmail.com>
2026-04-06 19:36:40 -0300

    database.db como sample para teste e generate.sql modificado para criar mais dados de teste

--- Firmware/data-backup/generate.sql
+++ Firmware/data-backup/generate.sql
@@ -1,14 +1,16 @@
+DELETE FROM sample; DELETE FROM sqlite_sequence WHERE name='sample';
+
 INSERT INTO sample (timestamp, sampling_time, in_flow, out_flow)
 WITH RECURSIVE
   cnt(n) AS (
      SELECT 1
      UNION ALL
-     SELECT n + 1 FROM cnt WHERE n < 100 -- Define aqui quantas linhas quer gerar
+     SELECT n + 1 FROM cnt WHERE n < 1440*6 -- Define aqui quantas linhas quer gerar
   )
 SELECT 
     -- Gera datas retroativas (ex: agora menos n segundos)
-    datetime('now', '-' || (n * 10) || ' seconds'), 
+    datetime('now', '-' || (n * 60*10) || ' seconds'), 
     10, 
-    (ABS(RANDOM()) % 2), -- Simula o (RANDOM > 0.5) gerando 0 ou 1
-    (ABS(RANDOM()) % 2)
+    (ABS(RANDOM()) % 10001), -- Simula o (RANDOM > 0.5) gerando 0 ou 1
+    (ABS(RANDOM()) % 10001)
 FROM cnt;
