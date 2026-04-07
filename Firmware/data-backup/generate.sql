dd3f55d6c309030f200a7b919c9eceb67b723764
AchcarLucas <achcarlucas@gmail.com>
2026-04-07 09:52:57 -0300

    - modificação no datebase para gerar a dados cada 20 minutos

--- Firmware/data-backup/generate.sql
+++ Firmware/data-backup/generate.sql
@@ -5,10 +5,11 @@ WITH RECURSIVE
   cnt(n) AS (
      SELECT 1
      UNION ALL
-     SELECT n + 1 FROM cnt WHERE n < (168*2)
+     SELECT n + 1 FROM cnt WHERE n < (503 * 2) -- 504 * 2 registros (7*24*3 * 2), sendo 20 minutos cada registro
   )
 SELECT 
-    datetime('now', printf('-%d hours', (168*2) - n)),
+    -- 10080 é o total de minutos em 7 dias (7 * 24 * 60) e 20 minutos cada registro
+    datetime('now', printf('-%d minutes', (7 * 24 * 60 * 2) - (n * 20))),
     10, 
     (ABS(RANDOM()) % 10001), 
     (ABS(RANDOM()) % 10001)
